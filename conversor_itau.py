7c0155c2d5632416f091a2f3c33a1e2760fefcc3
DuuFglitch <danielbessaribeiro@hotmail.com>
2024-09-09 10:57:47 -0300

    Update and rename extratoconsolidado.py to conversor_itau.py

--- conversor_itau.py
+++ conversor_itau.py
@@ -18,26 +18,57 @@ class PDFTableExtractor:
             raise ValueError("Nenhuma página válida foi especificada.")
         
         page_numbers = self.parse_pages(pages_with_tables)
-        header = pd.DataFrame()
+        main = pd.DataFrame()
 
         if '1' in page_numbers:
             header = self.get_table_data('page_1', '1')
             page_numbers.remove('1')
-            header_csv_path = os.path.join(self.csv_path, "header_page_1.csv")
-            header.to_csv(header_csv_path, sep=";", index=False, encoding='utf-8')
-        
-        main = pd.DataFrame()
+            
+            if not header.empty:
+                header = self.clean_data(header)
+                main = pd.concat([main, header], ignore_index=True)
+
         for i in range(0, len(page_numbers), 5):
             pages_block = page_numbers[i:i+5]
             block_data = self.get_table_data('page_2_end', ','.join(pages_block))
-            main = pd.concat([main, block_data], ignore_index=True)
-        
-        if not main.empty and not header.empty:
-            main = self.add_infos(header, main)
+            
+            if not block_data.empty:
+                block_data = self.clean_data(block_data)
+                self.debug_dataframes(main, block_data)
+                main = pd.concat([main, block_data], ignore_index=True)
+
+        if not main.empty:
             main = self.sanitize_column_names(main)
-            main = self.fill_empty_column(main, '144,262, 204,262, 157,757, 173,757')
+            main = self.fill_empty_dates(main, 'data')
+            main = self.remove_credit_debit_repeats(main)
+
             final_csv_path = self.save_csv(main)
-            self.combine_and_align_csvs(header_csv_path, final_csv_path)
+            self.finalize_csv(final_csv_path)
+
+    def clean_data(self, df):
+        df = df.reset_index(drop=True)  # Garante que os índices sejam únicos
+        df = df.loc[:, ~df.columns.duplicated()]  # Remove colunas duplicadas
+        df = df.dropna(axis=1, how='all')  # Remove colunas vazias
+        df.columns = df.columns.str.strip()  # Remove espaços extras dos nomes das colunas
+        
+        # Adiciona mais algumas limpezas específicas para as colunas
+        if 'data' in df.columns:
+            df['data'] = df['data'].str.strip()  # Remove espaços extras dos valores das colunas
+
+        return df
+
+    def debug_dataframes(self, df1, df2):
+        print("DataFrame principal:")
+        print("Colunas:", df1.columns)
+        print("Índices:", df1.index)
+        print("Exemplo de dados:")
+        print(df1.head())
+
+        print("\nDataFrame do bloco:")
+        print("Colunas:", df2.columns)
+        print("Índices:", df2.index)
+        print("Exemplo de dados:")
+        print(df2.head())
 
     def parse_pages(self, pages):
         page_numbers = []
@@ -61,7 +92,7 @@ class PDFTableExtractor:
             strip_text=config.get('strip_text', '')
         )
 
-        table_data = [self.fix_header(table.df) for table in tables if not table.df.empty]
+        table_data = [self.fix_header(table.df).reset_index(drop=True) for table in tables if not table.df.empty]
         return pd.concat(table_data, ignore_index=True) if table_data else pd.DataFrame()
 
     def save_csv(self, df):
@@ -84,54 +115,47 @@ class PDFTableExtractor:
 
     @staticmethod
     def sanitize_column_names(df):
-        df.columns = df.columns.map(lambda x: unidecode(x))  
+        df.columns = df.columns.map(lambda x: unidecode(str(x)))  
         df.columns = df.columns.map(lambda x: re.sub(r'[^\w\s]', '', x))  
         df.columns = df.columns.map(lambda x: x.replace(' ', '_'))  
         df.columns = df.columns.map(lambda x: x.lower())  
-        
+
+        df = df.loc[:, ~df.columns.duplicated()]  # Remove colunas duplicadas
         df = df.loc[:, ~df.columns.str.contains(r'^Unnamed:\s*\d+', regex=True)]
         if 'data_de_insercao' in df.columns:
             df = df.drop('data_de_insercao', axis=1)
         df = df.dropna(axis=1, how='all')
         return df
 
-    @staticmethod
-    def fill_empty_column(df, column_name):
-        if column_name not in df.columns:
-            return df
-        
-        for index, row in df.iterrows():
-            if pd.isna(row[column_name]):
-                last_valid_index = row.last_valid_index()
-                if last_valid_index and last_valid_index != column_name:
-                    df.at[index, column_name] = row[last_valid_index]
+    def fill_empty_dates(self, df, date_column_name):
+        if date_column_name in df.columns:
+            df[date_column_name] = df[date_column_name].replace('', pd.NA)  # Substitui strings vazias por NA
+            df[date_column_name] = df[date_column_name].ffill()  # Preenche valores vazios com a última data válida
         return df
 
-    def combine_and_align_csvs(self, header_csv, final_csv):
-        # Leia os arquivos CSV
-        header_df = pd.read_csv(header_csv, sep=";", encoding='utf-8')
-        final_df = pd.read_csv(final_csv, sep=";", encoding='utf-8')
+    def remove_credit_debit_repeats(self, df):
+        credit_column = 'credito'
+        debit_column = 'debito'
 
-        # Combine os DataFrames
-        combined_df = pd.concat([header_df, final_df], ignore_index=True)
+        if credit_column in df.columns:
+            df[credit_column] = df[credit_column].replace('', pd.NA)
+            df[credit_column] = df[credit_column].bfill()
+        
+        if debit_column in df.columns:
+            df[debit_column] = df[debit_column].replace('', pd.NA)
+            df[debit_column] = df[debit_column].bfill()
 
-        # Remove qualquer coluna completamente vazia
-        combined_df = combined_df.dropna(axis=1, how='all')
+        df = df.drop_duplicates()
+        return df
 
-        # Define o caminho do CSV final alinhado
-        final_aligned_path = os.path.join(self.csv_path, "extratoconsolidado.csv")
-
-        # Corrige o problema de múltiplos delimitadores consecutivos
-        combined_df.to_csv(final_aligned_path, sep=";", index=False, encoding='utf-8', na_rep='')
-
-        # Remover múltiplos delimitadores consecutivos do arquivo CSV resultante
-        with open(final_aligned_path, 'r', encoding='utf-8') as file:
+    def finalize_csv(self, final_csv):
+        final_aligned_path = os.path.join(self.csv_path, "extratoconvertido.csv")
+        
+        with open(final_csv, 'r', encoding='utf-8') as file:
             csv_content = file.read()
 
-        # Substitui múltiplos delimitadores `;` consecutivos por um único delimitador
         csv_content = re.sub(r';{2,}', ';', csv_content)
 
-        # Salva o conteúdo corrigido de volta ao arquivo
         with open(final_aligned_path, 'w', encoding='utf-8') as file:
             file.write(csv_content)
 
@@ -147,12 +171,12 @@ if __name__ == "__main__":
             'page_1': {
                 'table_areas': ['149,257, 552,21'],
                 'columns': ['144,262, 204,262, 303,262, 351,262, 406,262, 418,262, 467,262, 506,262, 553,262'],
-                'fix': True
+                'strip_text': ''
             },
             'page_2_end': {
                 'table_areas': ['151,760, 553,20'],
                 'columns': ['157,757, 173,757, 269,757, 309,757, 363,757, 380,757, 470,757, 509,757, 545,757'],
-                'fix': True
+                'strip_text': ''
             }
         }
         
